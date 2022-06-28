086ee7f3bd30358514c7c0ce5eababe99fda397d
LEO GOUBARD <goubardleo@gmail.com>
2022-06-28 15:10:22 +0200

    :hammer: server side, auth done

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth.js');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+require('dotenv').config;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use('/auth', authRoutes)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
